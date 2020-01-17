1c1ee8b002ffd59fcc28057aeb0244eb64dd142f
maxmedia <ognian.apostolov@gmail.com>
2020-01-17 16:09:30 +0200

    Comments in doctor page

--- src/routers/admin.js
+++ src/routers/admin.js
@@ -3,10 +3,11 @@ const express = require('express');
 const Doctor = require('../models/doctor');
 const User = require('../models/user');
 const Media = require('../models/media');
+const admin_auth = require('../middleware/admin-auth');
 
 const router = new express.Router();
 
-router.get('/admin-media', async (req, res) => {
+router.get('/admin-media', admin_auth, async (req, res) => {
     try {
         const medias = await Media.find({}).sort({'createdAt': 'descending'});
         
